79d15a63e71cd945844a4b29f81fb9884a772681
TroJanRuLes <andrey321861@gmail.com>
2025-06-15 17:39:25 +0300

    commit

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-const readlineSync = require('readline-sync');
+import welcomeUser from '../src/cli.js'
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!')
 
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+const name = welcomeUser()
+
+console.log(`Hello, ${name}!`)
